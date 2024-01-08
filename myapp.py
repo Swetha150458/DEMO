b77bf1ea0df86c25f9e520b4e6b5526bc1814552
Swetha150458 <132868930+Swetha150458@users.noreply.github.com>
2024-01-08 11:51:11 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,9 +5,9 @@ st.header("types of flowers")
 col1,col2=st.column(2)
 with col1:
   st.subheader("Daisy")
-  st.image("C:\Users\NIHARIKA\Desktop\daisy.jpg",caption="Daisy flower",width=300,use_column_width=True)
+  st.image("./daisy.jpg",caption="Daisy flower",width=300,use_column_width=True)
   st.write("Daisy flowers sre beautiful")
 with col2:
   st.subheader("Lily")
-  st.image("C:\Users\NIHARIKA\Desktop\lily.jpg",caption="lily flower",width=300,use_column_width=True)
+  st.image("./lily.jpg",caption="lily flower",width=300,use_column_width=True)
   st.write("lily flowers sre awsome")
